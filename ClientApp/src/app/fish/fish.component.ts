f84fc173a7ec5f49d78de9e31c592bacea01ba6c
seriouschess <rehayemb@gmail.com>
2020-05-05 15:25:58 -0500

    the fish can swim

--- ClientApp/src/app/fish/fish.component.ts
+++ ClientApp/src/app/fish/fish.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { inflate } from 'zlib';
 
 @Component({
   selector: 'app-fish',
@@ -9,32 +11,128 @@ export class FishComponent implements OnInit {
 
   constructor() { }
 
-  left_pos: number;
-  color_var: string;
+  @Input() left_pos: number;
+  @Input() top_pos: number;
+  direction: number;
+  frame_count: number; //frame leek issue??
+  oscilation: number;
+  width:number;
+  turn: boolean; //will the fish turn?
+  speed:number;
+
+  //related to feeding
+  has_target: boolean;
+  @Input() target_top: number;
+  @Input() target_left: number;
+  //color_var: string;
+
+  private eventsSubscription: Subscription;
+
+  @Input() events: Observable<number[]>;
 
   ngOnInit() {
-    this.left_pos = 0;
-    this.color_var = `rgb(${0},${0},${0})`;
+    this.has_target = false;
+    this.target_top = 0;
+    this.target_left = 0;
+    this.width = 100;
+    this.oscilation = 0;
+    this.frame_count = 0;
+    this.direction = 1;
+    this.speed = 3;
+    this.turn = false;
+    //this.color_var = `rgb(${0},${0},${0})`;
+    this.eventsSubscription = this.events.subscribe((f_loc:number[]) => this.seeFood(f_loc));
     this.update();
+  }
+
+  ngOnDestroy() {
+    this.eventsSubscription.unsubscribe();
+  }
+
+  seeFood(f_loc){
+    console.log("food!");
+    this.target_top = f_loc[1];
+    this.target_left = f_loc[0];
+    console.log(this.target_top +" "+this.target_left);
+    this.has_target = true;
+    this.speed = 6;
   }
 
   update(){
     setInterval(() => {
-      let r =  Math.floor((Math.random() * 256));
-      let g =  Math.floor((Math.random() * 256));
-      let b =  Math.floor((Math.random() * 256));
-      this.color_var =  `rgb(${r},${g},${b})`;
-      this.left_pos += 10;
-    },200);
+      // let r =  Math.floor((Math.random() * 256));
+      // let g =  Math.floor((Math.random() * 256));
+      // let b =  Math.floor((Math.random() * 256));
+      // this.color_var =  `rgb(${r},${g},${b})`;
+
+      this.frame_count += 1;
+      if( this.has_target ){
+        if( this.left_pos > this.target_left && this.direction == 1){
+          this.turn = true;
+        }else if( this.left_pos < this.target_left && this.direction == -1 ){
+          this.turn = true;
+        }
+
+        if( this.top_pos > this.target_top + this.speed ){
+          this.top_pos -= this.speed;
+        }
+        
+        if( this.top_pos < this.target_top - this.speed ){
+          this.top_pos += this.speed;
+        }
+      }else{
+        if( this.left_pos > 800 && this.direction == 1){
+          this.turn = true;
+        }else if( this.left_pos < 300 && this.direction == -1 ){
+          this.turn = true;
+        }
+      }
+
+      this.left_pos += this.speed*this.direction;
+      this.oscilation = 10*Math.sin(0.03*this.speed*this.frame_count);
+      this.turnFish();
+    },30);
+  }
+
+  turnFish(){
+    if(this.width > 0 && this.turn == true){ //turn fish
+      this.width -= 10;
+    }
+    
+    if(this.turn == true && this.width <= 0){ //width == 0, change direction
+      this.direction *= -1;
+      this.turn = false;
+    }
+
+    if(this.width < 100 && this.turn == false){ //finish turn
+      this.width += 10;
+      if(this.width > 100){
+        this.width = 100;
+      }
+    }
   }
 
   updateStyles() {
     let styles = {
-      'background-color' : this.color_var,
-      'left': `${this.left_pos}px`
+      //'background-color' : this.color_var,
+      'left': `${this.left_pos}px`,
+      'top': `${this.top_pos + this.oscilation}px`,
+      'transform': `scaleX(${this.direction}`,
+      'width': `${this.width}px`
     };
 
     return styles;
-} 
+  } 
+
+  baitFish(event){
+
+    var x = event.x;
+    var y = event.y;
+    var offsetX = event.offsetX;
+    var offsetY = event.offsetY;
+
+    //mouse data
+    console.log(event, x, y, offsetX, offsetY);
+  };
 }
 
--- ClientApp/src/app/tank/tank.component.ts
+++ ClientApp/src/app/tank/tank.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tank',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TankComponent implements OnInit {
 
+  food_top: number;
+  food_left: number;
+  eventsSubject: Subject<number[]> = new Subject<number[]>();
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  baitFish(event){
+    var x = event.x;
+    var y = event.y;
+    var offsetX = event.offsetX;
+    var offsetY = event.offsetY;
+    console.log(event, x, y, offsetX, offsetY);
+    this.food_top = event.x;
+    this.food_left = event.y;
+    this.emitEventToChild(this.food_top, this.food_left);
   }
 
+  emitEventToChild(x:number, y:number) {
+    this.eventsSubject.next([x,y]);
+  }
 }
